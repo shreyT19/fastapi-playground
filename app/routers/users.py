c87d657f5613b80efa3be428977c0a91f44533a0
shreyt19 <shreyansh@zenskar.com>
2024-03-30 10:16:18 +0530

    fix: raise exception for existing duplicate email id

--- app/routers/users.py
+++ app/routers/users.py
@@ -17,6 +17,9 @@ async def create_user(user: UserCreate,db: Session = Depends(get_db)):
     hashed_password = get_password_hash(user.password)
     user.password = hashed_password
     new_user = models.Users(**user.dict())
+    # we also need to check if same email already exists in the database
+    if db.query(models.Users).filter(models.Users.email == user.email).first():
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with same Email already exists")
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
